c7c0d9bf95202393d90e153cd9c2847f623fb5d7
nyanSpruk <nik.janspruk@gmail.com>
2024-03-30 18:57:32 +0100

    had wrong line heihgs

--- src/pages/BlogPreviewCard/assets/BlogPreviewCard.tsx
+++ src/pages/BlogPreviewCard/assets/BlogPreviewCard.tsx
@@ -8,19 +8,19 @@ function BlogPreviewCard() {
           alt="img"
         />
         <div className="font-figtree grid gap-3">
-          <p className="w-fit rounded bg-[hsl(47,88%,63%)] px-3 py-1 text-xs font-extrabold leading-6 sm:text-sm">
+          <p className="w-fit rounded bg-[hsl(47,88%,63%)] px-3 py-1 text-xs font-extrabold leading-[0.75*1.5] sm:text-sm sm:leading-[1.5*0.875rem]">
             Learning
           </p>
-          <p className="text-xs font-medium leading-6 sm:text-sm">
+          <p className="text-xs font-medium leading-[0.75*1.5] sm:text-sm sm:leading-[1.5*0.875rem]">
             Published 21 Dec 2023
           </p>
           <a
             href="/"
-            className="text-xl font-extrabold leading-6 hover:text-[hsl(47,88%,63%)] sm:text-2xl"
+            className="text-xl font-extrabold leading-[1.5*1.25] hover:text-[hsl(47,88%,63%)] sm:text-2xl sm:leading-[1.5*1.5]"
           >
             HTML & CSS foundations
           </a>
-          <p className="text-sm leading-6 text-[hsl(0,0%,50%)] sm:text-base">
+          <p className="text-sm leading-[1.5*0.875rem] text-[hsl(0,0%,50%)] sm:text-base">
             These languages are the backbone of every website, defining
             structure, content, and presentation.
           </p>
@@ -31,7 +31,9 @@ function BlogPreviewCard() {
             src="/blogPreviewCard/image-avatar.webp"
             alt="user"
           />
-          <p className="text-sm font-extrabold leading-6">Greg Hooper</p>
+          <p className="text-sm font-extrabold leading-[1.5*0.875rem]">
+            Greg Hooper
+          </p>
         </div>
       </div>
     </div>
