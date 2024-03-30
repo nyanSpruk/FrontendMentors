429314968689bfb83b5b5624d6b8157dd45af2b3
nyanSpruk <nik.janspruk@gmail.com>
2024-03-30 18:46:04 +0100

    font changes

--- src/pages/BlogPreviewCard/assets/BlogPreviewCard.tsx
+++ src/pages/BlogPreviewCard/assets/BlogPreviewCard.tsx
@@ -1,24 +1,26 @@
 function BlogPreviewCard() {
   return (
     <div className="flex h-screen items-center justify-center bg-[hsl(47,88%,63%)]">
-      <div className="m-6 flex w-full flex-col gap-6 rounded-2xl border border-black bg-white stroke-2 p-6 drop-shadow-[8px_8px_0_rgba(0,0,0,1)] sm:w-96">
+      <div className="sm:h- m-6 flex w-full flex-col gap-6 rounded-2xl border border-black bg-white stroke-2 p-6 drop-shadow-[8px_8px_0_rgba(0,0,0,1)] sm:w-96">
         <img
           className="h-[200px] rounded-xl object-cover"
           src="/blogPreviewCard/illustration-article.svg"
           alt="img"
         />
         <div className="font-figtree grid gap-3">
-          <p className="w-fit rounded bg-[hsl(47,88%,63%)] px-3 py-1 text-xs font-extrabold leading-6">
+          <p className="w-fit rounded bg-[hsl(47,88%,63%)] px-3 py-1 text-xs font-extrabold leading-6 sm:text-sm">
             Learning
           </p>
-          <p className="text-xs font-medium leading-6">Published 21 Dec 2023</p>
+          <p className="text-xs font-medium leading-6 sm:text-sm">
+            Published 21 Dec 2023
+          </p>
           <a
             href="/"
-            className="text-xl font-extrabold leading-6 hover:text-[hsl(47,88%,63%)]"
+            className="text-xl font-extrabold leading-6 hover:text-[hsl(47,88%,63%)] sm:text-2xl"
           >
             HTML & CSS foundations
           </a>
-          <p className="text-sm leading-6 text-[hsl(0,0%,50%)]">
+          <p className="text-sm leading-6 text-[hsl(0,0%,50%)] sm:text-base">
             These languages are the backbone of every website, defining
             structure, content, and presentation.
           </p>
